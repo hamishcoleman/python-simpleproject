4e98c512d8bfa71c64bca7edc07ee73389eea9cc
Hamish Coleman <hamish@zot.org>
2018-07-04 13:11:13 +0800

    Bug: make the example program actually run - how embarrasing!

--- theprogram.py
+++ theprogram.py
@@ -6,8 +6,6 @@
 import os
 import sys
 
-import example
-
 # Ensure that we look for any modules in our local lib dir.  This allows simple
 # testing and development use.  It also does not break the case where the lib
 # has been installed properly on the normal sys.path
@@ -15,6 +13,9 @@ sys.path.insert(0,
                 os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
                 )
 # I would use site.addsitedir, but it does an append, not insert
+
+# Import modules from our local dev dir
+import example
 
 if __name__ == '__main__':
     print(example.hellostr())
